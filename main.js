211a0f237e2c39f4f3420f3e539032f919a17b90
Artem Lipchansky <lipchanskiy.an@gmail.com>
2024-02-09 22:14:25 +0200

    aded

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-let students = 100;
-students = students + 50;
-students += 50;
-console.log(students);
+// let students = 100;
+// students = students + 50;
+// students += 50;
+// console.log(students);
+
+
+// const colors = ['tomato', 'teal', 'orange', 'deeplink', 'skyblue'];
+// const max = colors.lenght - 1;
+// const min = 0;
+
+// const index = Math.round(Math.random() * (max - min) + min);
+
+// const color = colors[index];
+
+// console.log(color);
+
+// document.body.style.backgroundColor = color;
+
+
+for (let i = 10; i >= 0; i -= 3) {
+    console.log(i);
+}
+
+console.log("bgbtb");
+
+
